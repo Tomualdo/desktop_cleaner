a1e05a7fa46b9017abeb4edeb38dafc13b698fb9
Tomas Krajcoviech <tkrajcoviech@sjsk.local>
2020-12-10 08:28:08 +0100

    first commit

---
+++ cleandesk.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python3
+
+from pathlib import Path
+from time import sleep
+import logging
+
+from watchdog.observers import Observer
+from watchdog.events import LoggingEventHandler
+
+from EventHandler import EventHandler
+
+if __name__ == '__main__':
+    logging.basicConfig(filename='cleaner.log',level=logging.DEBUG,
+                        format='%(asctime)s - %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+    # watch_path = Path.home() / 'Desktop'
+    watch_path = Path.home() / 'Documents/test_clean'
+
+    # destination_root = Path.home() / 'Desktop/holder'
+    destination_root = Path.home() / 'Documents/test_clean/holder'
+
+    loging_event_handler = LoggingEventHandler()
+    event_handler = EventHandler(watch_path=watch_path, destination_root=destination_root)
+
+    observer = Observer()
+    observer.schedule(event_handler, f'{watch_path}', recursive=False)
+    observer.schedule(loging_event_handler, f'{watch_path}', recursive=False)
+    observer.start()
+
+    try:
+        while True:
+            sleep(10)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
+
+#nssm.exe install "clean_desk" "C:\Users\tkrajcoviech\Documents\desktop_cleaner\.cleaner_env\Scripts\python.exe" "C:\Users\tkrajcoviech\Documents\desktop_cleaner\cleandesk.py"
---
+++ example_start.bat
@@ -0,0 +1,4 @@
+rem to setup windows service
+rem nssm.exe install "clean_desk" C:'Users\......\start.bat
+call ../.venv/Scripts/Activate.ps1
+call python cleandesk.py
